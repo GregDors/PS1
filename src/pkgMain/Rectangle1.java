8b1dcafe90034887f70f6c790021bef01438a0db
gregdorsey <gregdorsey@wifi-roaming-128-4-111-194.host.udel.edu>
2016-09-02 16:28:25 -0400

    Initial Checkin

---
+++ src/pkgMain/Rectangle1.java
@@ -0,0 +1,24 @@
+package pkgMain;
+
+public class Rectangle1 {
+
+	private double Length;
+	private double Width;
+	public Rectangle1(double length, double width) {
+		Length = length;
+		Width = width;
+	}
+	public double getLength() {
+		return Length;
+	}
+	public double getWidth() {
+		return Width;
+	}
+	
+	public double Area()
+	{
+		// TODO Implement Area() function with the correct formula
+		return Length * Width;
+		
+	}	
+}
